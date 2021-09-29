1a1382b5c3b7bfc59e9f3b29b3ec1a1a542d13ef
rnhub <dayal.rn@gmail.com>
2021-09-29 18:12:58 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./styles.css";
 
-function App() {
+const emojiDistinory = {
+  "🦍": "Gorilla",
+  "🐕": "Dog",
+  "🐈": "Cat",
+  "🦁": "Lion",
+  "🐄": "Cow",
+  "🐘": "Elephant",
+  "🐁": "Rat",
+  "🐇": "Rabbit",
+  "🦚": "Peackock",
+  "🦋": "Butterfly",
+  "🦜": "Parrot"
+};
+
+const emojiList = Object.keys(emojiDistinory);
+
+export default function App() {
+  const [meaning, setMeaning] = useState("");
+
+  function inputEventHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDistinory[userInput];
+
+    if (meaning === undefined) {
+      meaning = "Sorry! This emoji is not availabel in our database";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDistinory[emoji];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navigation">
+        <h1>Emoji Interpreter</h1>
+      </nav>
+      <input placeholder="Put emoji here" onChange={inputEventHandler} />
+      <div>{meaning}</div>
+      <p>Available Emojis</p>
+      {emojiList.map((item) => {
+        return (
+          <span
+            className="emojiSpan"
+            key={item}
+            onClick={() => emojiClickHandler(item)}
+          >
+            {item}
+          </span>
+        );
+      })}
     </div>
   );
 }
-
-export default App;
